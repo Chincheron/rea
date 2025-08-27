d1ab49ca0ab878cb4149655b361c25be074d7746
Murray Hyde <jmhyde891@gmail.com>
2025-08-26 23:42:23 -0400

    Minimum working product - build out from here

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import file_util
+import xlwings as xw
 import openpyxl
 import logging
 
@@ -8,29 +9,35 @@ def main():
     BASE_DIR = Path(__file__).resolve().parents[0]
     print(BASE_DIR)
     
-    #mount google drive
-    file_util.mount_drive('G:', '/mnt/g')
-
     #copy current REA version file 
-    file_util.copy_input_files('/mnt/g/My Drive/Paper Prep/Leslie Matrix/Versions/Current Working Version', BASE_DIR)
+    file_util.copy_input_files('G:\My Drive\Paper Prep\Leslie Matrix\Versions\Current Working Version', BASE_DIR)
 
     input_file = 'NEW CorrectedLeslieMatrix.2025.08.26.v1.2.xlsx'
-    wb_rea = openpyxl.load_workbook(input_file, data_only=False)
 
-    inputs_sheet = wb_rea['Debit Inputs']
-    outputs_sheet = wb_rea['Debit Inputs']
+    wb_rea = xw.Book(input_file)
 
-    scenario_one = 'Scenario_1'
+    sheet = wb_rea.sheets['Debit Inputs']
 
-    max_age_input = 'M12'
+    max_age = 50
 
-    max_age_value = 10
+    sheet['M12'].value = max_age
 
-    print(f'Running {scenario_one}')
+    wb_rea.app.calculate()
 
-    inputs_sheet[max_age_input] = max_age_value
+    #output next - need 1)direct DMSY lsot 2) Indirect DMSY lost 3) Totl DMSY Lost 4) DMSY Restored and 5) annual reelase over 1o eyars
+    #last one will be trickiest need to solve for it each time?
 
-    wb_rea.calculation()
+    #outputs to variables
+    direct_loss_total = sheet['T3'].value
+    print(direct_loss_total)
+
+    #save results
+    with open('results.csv', 'w') as f:
+        f.write('input,output\n')
+        f.write(f'{max_age}, {direct_loss_total}\n')
+
+
+    wb_rea.app.quit()
     
 if __name__ == "__main__":
     main()
