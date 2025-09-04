c8eb15b1dcf79a2224ebb8f7a8f715ea1f2be693
Murray Hyde <jmhyde891@gmail.com>
2025-09-04 11:44:28 -0400

    feat: add load excel sheet function to module

--- excel_util.py
+++ excel_util.py
@@ -1,6 +1,7 @@
 '''Define functions for manipulating excel files using xlwings'''
 import xlwings as xw
 from pathlib import Path
+import logging
 
 def load_workbook(workbook: Path |str, visible: bool = False) -> tuple[xw.Book, xw.App]:
     '''Loads Excel file using xlwing in background (headless) mode'''
@@ -16,3 +17,16 @@ def load_workbook(workbook: Path |str, visible: bool = False) -> tuple[xw.Book, 
     except Exception as e:
         app.quit()
         raise e
+    
+def load_worksheet(wb: xw.Book, sheet_name: str, logger: logging.Logger | None = None) -> xw.Sheet:
+    '''Loads specificed worksheet from xlwings workbook'''
+
+    if logger is None:
+        logger = logging.getLogger(__name__)
+
+    try:
+        return wb.sheets[sheet_name]
+    except Exception as e:
+        msg = f'Sheet {sheet_name} not found in workbook {wb}'
+        logger.error(msg, exc_info=e)
+        raise ValueError(msg) from e
--- test_excel_util.py
+++ test_excel_util.py
@@ -4,12 +4,12 @@ from pathlib import Path
 excel_path = Path('test_inputs') / 'Mussel REA_v2.0.1.xlsx'
 
 wb, app = excel_util.load_workbook(excel_path)
-
 print(type(wb))
-
 print(type(app))
 
-wb, app = excel_util.load_workbook("test")
+sheet = excel_util.load_worksheet(wb, 'Matrix Inputs')
+print(type(sheet))
+#excel_util.load_worksheet(wb, ['test'])
 
 wb.close()
 app.quit()
