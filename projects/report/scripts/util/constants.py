9d4758d431bcf6531da077356c6eeb2561798c91
Murray Hyde <jmhyde891@gmail.com>
2025-09-07 23:52:22 -0400

    chore: save results ina working folder for easy cleanup if not finalized

--- projects/report/scripts/util/constants.py
+++ projects/report/scripts/util/constants.py
@@ -4,4 +4,4 @@ import util.file_util as file_util
 REPO_DIR = file_util.find_repository_root()
 PROJECT_BASE_DIR = (REPO_DIR / 'projects' / 'report')
 CONFIG_DIR = PROJECT_BASE_DIR / 'config'
-RESULTS_DIR = (PROJECT_BASE_DIR / 'results')
+RESULTS_DIR = (PROJECT_BASE_DIR / 'results' / 'working')
